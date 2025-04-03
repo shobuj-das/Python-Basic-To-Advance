9c8b6f3be719e43b9c7f4e615b30cb1691ca1f09
Shobuj Das <dasshobuj@gmail.com>
2025-04-04 01:26:58 +0600

    string

---
+++ String/Concatenate.py
@@ -0,0 +1,9 @@
+a = "My name "
+b = "is Shobuj"
+name = a + b
+print(name)
+
+x = "hello"
+y = "Dear"
+mgs = x + " " + y
+print(mgs)
---
+++ String/Format.py
@@ -0,0 +1,12 @@
+age = 32
+print(f"My age is {age}")
+
+txt = f"My age is about {age}"
+print(txt)
+
+price = 300
+txt = f"the price is near {price:.2f}"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)

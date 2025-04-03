03b4411b720f953d70bd0598a4d3f10d986388fe
Shobuj Das <dasshobuj@gmail.com>
2025-04-04 00:56:37 +0600

    string

---
+++ String/String.py
@@ -0,0 +1,23 @@
+name = "Shobuj"
+print(name)
+
+# name = input("Enter name: ")
+# print(name)
+
+print(type(name))
+
+story = """This is a multiple story
+written in python programming language.
+it will be displayed as it is.
+"""
+print(story)
+
+print(name[3])
+print(len(name))
+
+for x in name:
+    print(x)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")

9eca141957117dab5c384ed0a3a7fef7ec763760
Shobuj Das <dasshobuj@gmail.com>
2025-04-10 23:59:20 +0600

    array

---
+++ Collections(Arrays)/List.py
@@ -0,0 +1,28 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+print(mylist[1])
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+list1 = ["abc", 34, True, 40, "male"]
+
+print(list1)
+
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+print(type(thislist))
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])

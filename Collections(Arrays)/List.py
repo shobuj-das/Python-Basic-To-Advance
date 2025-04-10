3c419cbca26a4af3acf24d3bb7ecd4171d1b0e18
Shobuj Das <dasshobuj@gmail.com>
2025-04-11 00:35:31 +0600

    list

--- Collections(Arrays)/List.py
+++ Collections(Arrays)/List.py
@@ -26,3 +26,97 @@ print(thislist[2:5])
 
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+# ------- add values --------
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+thislist.append("hello")
+print(thislist)
+
+# --------- remove ---------------
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+
+thislist.extend(tropical)
+print(thislist)
+
+thislist.remove("apple")
+print(thislist)
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+mylist.clear()
+print(mylist)
+print(len(mylist))
+
+# ------ loop ----
+mylist = ["apple", "banana", "cherry"]
+for x in mylist:
+  print(x)
+
+for i in range(len(mylist)):
+    print(mylist[i])
+
+print(f'thislist:{thislist}')
+i =0;
+while i<len(thislist):
+    print(thislist[i])
+    i+=1
+
+[print(x) for x in thislist]
+
+# --- list comprehension ----
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)

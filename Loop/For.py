1b514d5e97c5626e5aff98efc525f3ab80b96b5a
Shobuj Das <dasshobuj@gmail.com>
2025-04-12 23:26:34 +0600

    for loop

---
+++ Loop/For.py
@@ -0,0 +1,16 @@
+for i in range(5):
+    print(i)
+
+for y in range(1,10):
+    print(y)
+
+for x in range(1,10,2):
+    print(x)
+
+str = "Hello I am shobuj das, a programmer"
+for ch in str:
+    print(ch)
+
+for word in str.split():
+    if word == "shobuj":
+        print(word)

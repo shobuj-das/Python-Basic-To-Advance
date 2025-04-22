3bb25e5abc67927414021900c2cec2928ac80e75
Shobuj Das <dasshobuj@gmail.com>
2025-04-22 23:48:11 +0600

    class added

---
+++ OOP/Basic.py
@@ -0,0 +1,15 @@
+class Demo:
+    x=10
+
+d = Demo()
+print(d.x)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)

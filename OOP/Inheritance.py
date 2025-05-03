4221fd0bb830d97243fc48b2490404fb9b212dbb
Shobuj Das <dasshobuj@gmail.com>
2025-05-03 23:46:11 +0600

    inheritance

---
+++ OOP/Inheritance.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def print_details(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+class Child (Person):
+    def __init__(self, name, age, address):
+        super().__init__(name, age)
+        self.address = address
+    def print_details(self):
+        print(f"Name: {self.name}, Age: {self.age}, Address: {self.address}")
+
+x = Person("John", 36)
+ch = Child("Jane", 25, "Pune")
+ch.print_details()
+x.print_details()

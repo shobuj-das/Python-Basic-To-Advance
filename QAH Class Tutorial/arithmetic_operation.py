ac9ef6bfe235e654624dc85bdec45c2cbb09784f
Shobuj Das <dasshobuj@gmail.com>
2025-06-22 00:32:09 +0600

    qah class code

---
+++ QAH Class Tutorial/arithmetic_operation.py
@@ -0,0 +1,10 @@
+a = 10
+b = 4
+
+print("Addition (a + b) = ", a+b)
+print("Substraction (a - b) = ", a-b)
+print("Multiplication (a * b) = ", a*b)
+print("Division (a / b) = ", a/b)
+print("Floor division (a // b) = ", a//b)
+print("Modulus (a % b) = ", a%b)
+print("Exponent (a ** b) = ", a**b)
---
+++ QAH Class Tutorial/assignment_operator.py
@@ -0,0 +1,19 @@
+num = 50
+print("Initial num : ",num)
+num+=10
+print("After +=10 : ", num)
+
+num -= 5
+print("After -=5 : ", num)
+
+num *= 2
+print("After *=2 : ", num)
+
+num /= 5
+print("After /=5 : ",num)
+
+num //=3
+print("After //=3 :",num)
+
+num **=3
+print("After **=3 : ",num)
